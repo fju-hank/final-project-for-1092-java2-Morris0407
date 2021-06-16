db2de67278e6cbbab3c543124ecfa745a64a2155
morris <406108957morris@gmail.com>
2021-06-17 01:04:09 +0800

    edit id and name

--- src/main/java/com/fju/Main.java
+++ src/main/java/com/fju/Main.java
@@ -1,23 +1,33 @@
 package com.fju;
 
+
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        List<MenuItem > menu =  new ArrayList<>( );
         try {
             BufferedReader br = new BufferedReader(new FileReader("Menu.txt"));
             String line = br.readLine();
             while (line != null){
                 String[] tks = line.split(",");
-
+            MenuItem item = new MenuItem(tks[0],tks[1],Integer.parseInt(tks[2]));
+            line = br.readLine();
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        for (int i = 0; i < menu.size(); i++) {
+            MenuItem item = menu.get(i);
+            System.out.println(item.getId() + " " + item.getName());
         }
     }
 }
--- src/main/java/com/fju/MenuItem.java
+++ src/main/java/com/fju/MenuItem.java
@@ -4,30 +4,27 @@ public class MenuItem {
     String id;
     String name;
     int price1;
-    int price2;
-    public MenuItem (){
-        super();
-    }
 
-    public  MenuItem (String id ,String name,int price1,int price2){
+    public  MenuItem(String id, String name, int price1){
         this.id = id;
         this.name =name;
         this.price1=price1;
-        this.price2 =price2;
 
     }
+
+
+
     public String getId(){
         return id;
     }
+
     public String getName(){
         return name;
     }
     public int getPrice1(){
         return price1;
     }
-    public int getPrice2(){
-        return price2;
-    }
+
 
 
 
